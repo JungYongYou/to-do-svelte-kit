8534d9e8c1214ae69c15156ff912874bd73db576
jydev <lukaryu@naver.com>
2023-01-25 12:15:51 +0900

    SubmitFunction을 통한 로딩 제어

--- src/routes/todos/+page.svelte
+++ src/routes/todos/+page.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-  import { enhance } from '$app/forms';
+  import { enhance, type SubmitFunction } from '$app/forms';
     import type { ActionData, PageData } from './$types';
 
     export let data: PageData;
     export let form: ActionData
+
+    let loading = false
+
+    const addTodo: SubmitFunction = () => {
+        loading = true;
+
+        return async ({update}) => {
+            loading = false;
+            await update()
+        }
+    }
 </script>
 
 <pre>
@@ -22,12 +33,12 @@
     {/each}
 </ul>
 
-<form method="POST" action="?/addTodo" use:enhance>
+<form method="POST" action="?/addTodo" use:enhance={addTodo}>
     <input type="text" name="todo" />
     {#if form?.missing}
         <p class="error">This field is required</p>
     {/if}
-    <button type="submit">+ Add Todo</button>
+    <button aria-busy={loading} class:secondary={loading} type="submit">{#if !loading}+ Add Todo{/if}</button>
 
     <button formaction="?/clearTodos" class="secondary" type="submit">Clear</button>
 </form>
